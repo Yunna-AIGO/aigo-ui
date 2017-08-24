36ce70048cf880d9d23784386f3a25dae205714a
AlexanderYao <dwy4670@gmail.com>
2017-08-24 08:45:51 +0800

    add todo list

--- app/tools/constants.js
+++ app/tools/constants.js
@@ -4,7 +4,7 @@ export const SUCCESS = '0000';
 // storages
 export const loginState = 'loginState';
 
-// mock环境：启动nodejs
+// mock环境：cd server & yarn run start
 const urlPrefix = 'http://localhost:9000/api/v1/';
 // 真实的服务接口：本地环境
 // const urlPrefix = 'http://localhost:8080/cloudpick/rest/api/v1/';
@@ -20,7 +20,7 @@ export const accountInfo = urlPrefix + 'account/{userId}/query'
 export const orders = urlPrefix + 'order/{userId}/list?pageNum={pageNum}&pageSize={pageSize}';
 export const orderInfo = urlPrefix + 'order/{orderId}/info';
 
-export const getUserInfo = urlPrefix + 'user/info/{userId}';
+export const userInfo = urlPrefix + 'user/getById/{userId}';
 export const updateUserInfo = urlPrefix + 'user/info';
 
 export const recharge = urlPrefix + 'trade/recharge';
